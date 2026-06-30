31cf1409819d9c988677f6de193f6f192a5a037f
Saksahm Katuwal <katuwalsaksham18@gmail.com>
2026-06-30 16:49:39 +0545

    Plotted the TS diagram

--- ThermoCycleSimulator/OttoCycle.py
+++ ThermoCycleSimulator/OttoCycle.py
@@ -12,20 +12,24 @@ Cv = float(input("Enter the value of sepcefic heat at constant valume in J/Kg*K 
 R = 287 #USpecefic gas constant for air :J/Kg*k
 m = 1 #per kg of air
 V1 = (m*R*T1)/P1
+S1 = 0 #reference
 # isentropic compression(Stage 1-2), piston is at top (State 2)
 T2 = T1 * (r_k ** (gamma - 1))
 P2 = P1 * (r_k**gamma)
 V2 = (m*R*T2)/P2
+S2 = S1 #Isentropic process
 
 # isochoric heat addition(Stage 2-3), fuel is just burnt,Piston at TDC (State 3)
 T3 = T2 + (Q_in / Cv)
 P3 = P2 * (T3 / T2)
 V3 = V2
+S3 = S2  + Cv* (np.log(T3/T2))  # Isochoric process: change in entropy = Cv *ln(T_final/T_initial)
 
 # isentropic expansion(Stage 3-4), piston at BDC (State 4)
 T4 = T3 / (r_k ** (gamma - 1))
 P4 = P3 / (r_k**gamma)
 V4 = (m*R*T4)/P4
+S4 = S3
 Efficiency = (1-(1/(r_k**(gamma-1))))*100
 # isochoric heat rejection(Stage 4-1)
 V12 = np.linspace(V1,V2,100) #100 equally spaced points between V1 and V2
@@ -37,6 +41,16 @@ P12 = P1* (V1**gamma) /(V12**gamma) #PV**gamma = constant
 P23 = np.array([P2,P3]) 
 P34 = P3* (V3**gamma) /(V34**gamma) 
 P41 = np.array([P4,P1]) 
+
+S12 = np.array([S1,S2])
+S23 = np.linspace(S2,S3, 100)
+S34 = np.array([S3,S4])
+S41 = np.linspace(S4,S1, 100)
+
+T12 = np.array([T1,T2]) 
+T23 = T2 * np.exp((S23 - S2)/Cv)
+T34 = np.array([T3,T4]) 
+T41 = T4 * np.exp((S41 - S4)/Cv)
 # Printing Values of temperature at different stages
 print(f"Initial temperature T1= {T1:.2f} K")
 print(f"After isentropic compression T2= {T2:.2f} K")
@@ -59,6 +73,19 @@ plt.plot(V41,P41, color ='m',linestyle = '--', label = '4->1 Isochoric Heat Reje
 plt.title("Otto cycle PV diagram", color = 'r')
 plt.xlabel('Volume(m^3/kg)')
 plt.ylabel('Pressure(Pa)')
+plt.grid(True , linestyle = '--')
+# plt.legend()
+# plt.show()
+
+#Plotting the TS diagram
+plt.figure(figsize=(10,8))
+plt.plot(S12,T12, color ='b',linestyle = '--', label = '1->2 Isentropic Compression')
+plt.plot(S23,T23, color ='g',linestyle = '--', label = '2->3 Isochoric Heat Addition')
+plt.plot(S34,T34, color ='r',linestyle = '--', label = '3->4 Isentropic Expansion')
+plt.plot(S41,T41, color ='m',linestyle = '--', label = '4->1 Isochoric Heat Rejection')
+plt.title("Otto cycle TS diagram", color = 'r')
+plt.xlabel('Entropy(J/K)')
+plt.ylabel('Temperature(K)')
 plt.grid(True , linestyle = '--')
 plt.legend()
 plt.show()
